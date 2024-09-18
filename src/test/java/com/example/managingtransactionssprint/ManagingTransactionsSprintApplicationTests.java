05d2a95489238ac9a4b0b4dcdcf8270ead9a3d7d
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-18 06:21:03 +0200

    initial commit

---
+++ src/test/java/com/example/managingtransactionssprint/ManagingTransactionsSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managingtransactionssprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingTransactionsSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
